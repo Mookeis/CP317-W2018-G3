1d08ad7bc19e6f7f192297081be2a603f0ba997a
Vindair <39199957+Vindair@users.noreply.github.com>
2018-08-07 23:28:14 -0400

    Fav bar functionality added

--- subby/views/favourite.py
+++ subby/views/favourite.py
@@ -1,16 +1,9 @@
-from django.http import Http404
-
-from django.http import HttpResponse, Http404 
 from django.shortcuts import render, get_object_or_404,redirect
-from django.db import models
-from django.views.generic import ListView, DetailView
 from subby.models.favourite import Favourite
 from subby.models.sublet import Sublet
 from subby.models.image import SubletImage
 from subby.decorators.loginrequiredmessage import message_login_required
 from django.contrib import messages
-
-
 
 	
 @message_login_required
@@ -30,17 +23,23 @@ def FavouriteLister(request):
 	return render(request, 'favourite/favourite_list.html', fav_dict)
     
 
+@message_login_required
+def favourite_bar_lister(request):
+    fav_list = Favourite.objects.filter(user=request.user)
+
+    image_list = []
+    for fav in fav_list:
+        images = SubletImage.objects.filter(sublet=fav.sublet)
+        image_list.append(images[0])
+
+    data = {
+             'fav_list': fav_list,
+             'image_list': image_list,
+            }
+
+    return render(request, 'favourite/favourite_nav.html', data)
 
 
-class FavouriteMenuBar(ListView):
-    def index(request):
-        all_favourites = Favourite.objects.all()
-        ctx = {'all_favourites': all_favourites}
-
-        return render(request, 'favourite/listings.html', ctx)
-
-
-# Favourites a sublet if not favourited, unfavourites a sublet if already favourited (deletes it from db)
 def fav_unfav_sublet(request):
 
     current_user = request.user
@@ -49,7 +48,6 @@ def fav_unfav_sublet(request):
     try:
         sublet = get_object_or_404(Sublet, id=request.POST['subletid'])
     except(KeyError, Sublet.DoesNotExist):
-        #return redirect('subby:SubletDetail', {'sublet': sublet, 'error': "Sublet does not exist."})
         return redirect(next, id=sublet.get_sublet_id())
     else:
         #If it exists check to see if this favourite already exists
