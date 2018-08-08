9100e3434a04f1333aa45ae6190f0d87827b87a7
Kuresov <alex.kirsopp@gmail.com>
2018-08-07 20:45:04 -0400

    Prod db test

--- subby_project/settings/production.py
+++ subby_project/settings/production.py
@@ -1,15 +1,12 @@
 from .settings import *
+import os
+import dj_database_url
 
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cp317-w2018-g3.herokuapp.com']
+
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DBNAME', ''),
-        'USER': os.environ.get('DBUSER', ''),
-        'HOST': os.environ.get('DBHOST', ''),
-        'PORT': os.environ.get('DBPORT', ''),
-        'PASSWORD': os.environ.get('DBPASS', ''),
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require = True)
 }
