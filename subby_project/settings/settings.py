311439e75cb76f34b42ff5f4d09e5b260e232ded
Xiaochao Luo <zack0813@live.cn>
2018-08-08 22:46:32 -0400

    Revert "Update settings.py"
    This reverts commit 97cad5d90fa6a1744ce8ca4dab087a05eeea5466.

--- subby_project/settings/settings.py
+++ subby_project/settings/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myApp',
+    'tinymce',
     'djcelery',
     # 'social_django',
 ]
