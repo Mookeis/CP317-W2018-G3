0edfd52297b06ab1ac3395f657b63ec3f1f1b90e
tom <39259926+tompeng123@users.noreply.github.com>
2018-08-08 21:03:58 -0400

    Update settings.py

--- subby_project/settings/settings.py
+++ subby_project/settings/settings.py
@@ -26,8 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myApp'
-    'tinymce'
+    'myApp',
+    'tinymce',
+    'djcelery',
     # 'social_django',
 ]
 
